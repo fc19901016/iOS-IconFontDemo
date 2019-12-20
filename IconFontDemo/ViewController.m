7a7ad1a577a1b0a7d06cb5b64cf481dae4e34908
fc19901016 <574993357@qq.com>
2019-12-20 17:05:29 +0800

    矢量文字
    矢量文字

--- IconFontDemo/ViewController.m
+++ IconFontDemo/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "TBCityIconFont.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView1;
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView2;
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView3;
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView4;
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView5;
+@property (weak, nonatomic) IBOutlet UIImageView *testImageView6;
 
 @end
 
@@ -16,7 +23,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view, typically from a nib.
+    self.testImageView1.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.redColor)];
+    self.testImageView2.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.yellowColor)];
+    self.testImageView3.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.blueColor)];
+    self.testImageView4.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.greenColor)];
+    self.testImageView5.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.blackColor)];
+    self.testImageView6.image =  [UIImage iconWithInfo:TBCityIconInfoMake(@"", 125, UIColor.cyanColor)];
+    
+    if ([@""isEqualToString:@""]) {
+        NSLog(@"杨洋喜欢泡妹子！");
+    }else{
+        NSLog(@"杨洋是大神！");
+    }
+    
 }
 
 
